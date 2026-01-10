16d06209262d92cf69b7abf04ddb7febe1477d7e
Adinath-Jagtap <adinathjagtap9702@gmail.com>
2026-01-10 22:11:51 +0530

    DAY 1 DB CREATION

---
+++ DAY-1/create-db.sql
@@ -0,0 +1,20 @@
+CREATE TABLE employees (
+    id INT,
+    name VARCHAR(50),
+    age INT,
+    city VARCHAR(50),
+    salary INT
+);
+
+INSERT INTO employees VALUES
+(1, 'Amit', 24, 'Mumbai', 40000),
+(2, 'Riya', 28, 'Delhi', 60000),
+(3, 'Ankit', 22, 'Pune', 35000),
+(4, 'Sneha', 30, 'Mumbai', 55000),
+(5, 'Arjun', 26, 'Delhi', 45000),
+(6, 'Neha', 21, 'Nagpur', 30000),
+(7, 'Aakash', 29, 'Bangalore', 70000),
+(8, 'Pooja', 23, 'Mumbai', 42000),
+(9, 'Rahul', 27, 'Chennai', 48000),
+(10, 'Aarti', 25, 'Delhi', 39000),
+(11, 'Vikas', 32, 'Pune', 80000);
