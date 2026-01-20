ea265ddac081a316c9163c0d2285e2569f843424
Adinath-Jagtap <adinathjagtap9702@gmail.com>
2026-01-20 23:22:34 +0530

    DAY-9

---
+++ DAY-9/DAY-9-SQL.sql
@@ -0,0 +1,8 @@
+-- =============================================================================
+-- SQL PROBLEMS (2 - EASY)
+-- =============================================================================
+/*
+1. Select employees with salary greater than 50000
+
+2. Count the number of employees in each department
+*/
