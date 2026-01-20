71429ed611162b2a62e3cbb73fabf217fbd490ff
Adinath-Jagtap <adinathjagtap9702@gmail.com>
2026-01-20 23:20:25 +0530

    CREATE DATABASE

---
+++ DAY-9/CREATE-DAY-9-DB.sql
@@ -0,0 +1,15 @@
+-- Sample table creation (optional)
+CREATE TABLE employees (
+  emp_id INTEGER PRIMARY KEY,
+  name TEXT,
+  dept_id INTEGER,
+  salary INTEGER
+);
+
+-- Sample inserts (optional)
+INSERT INTO employees VALUES
+ (1,'Amit',1,40000),
+ (2,'Riya',2,60000),
+ (3,'Ankit',2,35000),
+ (4,'Sneha',3,55000),
+ (5,'Arjun',3,45000);
